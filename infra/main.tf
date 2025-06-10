ba353c55dc5aeb97e4c62bed99b620f16772f42d
RodrigoSalvado <rodrigoapsalvado@gmail.com>
2025-06-10 18:19:27 +0100

    teste

--- infra/main.tf
+++ infra/main.tf
@@ -119,6 +119,7 @@ resource "azurerm_linux_web_app" "web" {
   resource_group_name = azurerm_resource_group.main.name
   location            = local.location
   service_plan_id     = azurerm_service_plan.main.id
+  os_type             = "Linux"
 
   site_config {
     always_on = true
